061fdf55167ee1d9a3c479ba7eff33aa555cfec4
Ronie Oljemark <ronie.oljemark@student.careeria.fi>
2022-05-06 11:38:48 +0300

    Employee Controllerin lisäys ym pientä.

---
+++ TimesheetBackend/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TimesheetBackend.Models;
+
+namespace TimesheetBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly tuntidbContext db = new tuntidbContext();
+
+
+        [HttpGet]
+
+        public List<Employee> GetAllActive()
+        {
+            var employees = db.Employees.Where(e => e.Active == true);
+            
+            return employees.ToList(); 
+        }
+
+    }
+}
